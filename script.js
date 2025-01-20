4de97826ccbcfe538377b2099b4edfff95b6297d
Lolah23 <143958464+Lolah23@users.noreply.github.com>
2025-01-20 13:00:07 +0200

    HTML, CSS, and JavaScript files

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(e.target.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
